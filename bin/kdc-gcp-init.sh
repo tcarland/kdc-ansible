6fc8354854a7aae59141868344b2a8aba6539043
tca <tcarland@gmail.com>
2019-09-09 10:42:16 -0700

    add mtype param

--- bin/kdc-gcp-init.sh
+++ bin/kdc-gcp-init.sh
@@ -5,13 +5,18 @@
 PNAME=${0##*\/}
 
 tdh_path="$1"
+mtype="$2"
 
 if [ -z "$tdh_path" ]; then
     echo "Usage: $PNAME [path/to/tdh-gcp]"
     exit 0
 fi
 
+if [ -z "$mtype" ]; then
+    mtype="n1-standard-1"
+fi
+
 # default hostnames as tdh-kdc01 and tdh-kdc02
-( tdh-gcp-compute.sh -t n1-standard-1 kdc01 kdc02 )
+( ${tdh_path}/bin/tdh-gcp-compute.sh -t $mtype kdc01 kdc02 )
 
 exit 0
