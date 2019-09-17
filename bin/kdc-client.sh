a062a62f4b1e116e9006b310837ff9bc58bc476a
tca <tcarland@gmail.com>
2019-09-17 10:48:34 -0700

    fix escaped quote

--- bin/kdc-client.sh
+++ bin/kdc-client.sh
@@ -8,7 +8,6 @@ shift
 hosts="$@"
 
 if [ -z "$inv" ] || [ -z "$hosts" ]; then
-    echo "No hosts provided"
     echo ""
     echo "Usage: $PNAME [inventory_name] [host1] <host2> <host3> ..."
     echo "Where [inventory_name] is the environment ./inventory/[name]"
@@ -18,7 +17,8 @@ fi
 
 hosts=$( echo $hosts | sed -e 's/[[:space:]][[:space:]]*/,/g' )
 
-echo "( ansible-playbook -i $inv kdc-clients.yml --extra-vars \"@inventory/$inv/group_vars/all/vars" )"
+# Run playbook with list of hosts as inventory
+echo "( ansible-playbook -i $inv kdc-clients.yml --extra-vars \"@inventory/$inv/group_vars/all/vars\" )"
 ( ansible-playbook -i $inv kdc-clients.yml --extra-vars "@inventory/$inv/group_vars/all/vars" )
 
 exit $?    
